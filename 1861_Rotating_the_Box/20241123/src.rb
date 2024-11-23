5e348103e53187d5d1fa06f35199c2e4628d9947
fvknk <97640891+fvknk@users.noreply.github.com>
2024-11-23 13:04:32 +0900

    1861. Rotating the Box

---
+++ 1861_Rotating_the_Box/20241123/src.rb
@@ -0,0 +1,27 @@
+# @param {Character[][]} box
+# @return {Character[][]}
+def rotate_the_box(box)
+  box.reverse!.map! do |b|
+    unique_strings = b.uniq
+    next b if !(unique_strings.include?('#') && unique_strings.include?('.'))
+
+    result = []
+    start_index = 0
+    asterisk_index = b.index('*')
+    next b.sort!.reverse! if asterisk_index.nil?
+
+    tmp = b
+    while asterisk_index
+      result.concat(tmp[...asterisk_index].sort!.reverse! << '*')
+
+      start_index = asterisk_index + 1
+      break if start_index == b.length
+
+      tmp = tmp[start_index..]
+      asterisk_index = tmp.index('*')
+    end
+    next result if start_index == b.length
+
+    result.concat(tmp[..-1].sort!.reverse!)
+  end.transpose
+end
