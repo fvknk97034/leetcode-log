d2eeb4853481074d70881d8cd8c939330e7ae5e8
fvknk <97640891+fvknk@users.noreply.github.com>
2024-12-02 21:31:07 +0900

    21. Merge Two Sorted Lists

---
+++ 21_Merge_Two_Sorted_Lists/20241202/src.rb
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode
+#   attr_accessor :val, :next
+#   def initialize(val = 0, _next = nil)
+#     @val = val
+#     @next = _next
+#   end
+# end
+# @param {ListNode} list1
+# @param {ListNode} list2
+# @return {ListNode}
+def merge_two_lists(list1, list2)
+  return list2 if list1.nil?
+  return list1 if list2.nil?
+
+  list1, list2 = [list2, list1] if list1.val > list2.val
+  node1, node2 = [list1, list2]
+
+  while !node1.nil? && !node2.nil?
+    if node1.next.nil?
+      node1.next = node2
+      break
+    end
+
+    next if node1.val > node2.val
+
+    if node1.next.val >= node2.val
+      tmp = node2.next
+
+      node2.next = node1.next
+      node1.next = node2
+      node1 = node1.next
+
+      node2 = tmp
+      next
+    end
+
+    node1 = node1.next
+  end
+  list1
+end
