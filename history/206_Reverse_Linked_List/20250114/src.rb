2877feabe506a65b7af5cd76dca8399a9d5a91bf
fvknk <97640891+fvknk@users.noreply.github.com>
2025-01-14 21:30:07 +0900

    206. Reverse Linked List

---
+++ history/206_Reverse_Linked_List/20250114/src.rb
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode
+#   attr_accessor :val, :next
+#   def initialize(val = 0, _next = nil)
+#     @val = val
+#     @next = _next
+#   end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def reverse_list(head)
+  return head if head.nil?
+
+  left = head
+  middle = head.next
+  right = head.next&.next
+  left.next = nil
+  until middle.nil?
+    middle.next = left
+
+    left = middle
+    middle = right
+    right = right&.next
+  end
+  left
+end
