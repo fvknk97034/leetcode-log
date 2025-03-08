82bf966e41e83aac394b7763251263f2c9f632b4
fvknk <97640891+fvknk@users.noreply.github.com>
2025-03-08 12:37:27 +0900

    610. Triangle Judgement

---
+++ 610_Triangle_Judgement/20250308/src.sql
@@ -0,0 +1,16 @@
+SELECT
+  *,
+  CASE
+    WHEN x >= y
+    AND x >= z
+    AND x < y + z THEN 'Yes'
+    WHEN y >= x
+    AND y >= z
+    AND y < x + z THEN 'Yes'
+    WHEN z >= x
+    AND z >= y
+    AND z < x + y THEN 'Yes'
+    ELSE 'No'
+  END AS triangle
+FROM
+  Triangle;
