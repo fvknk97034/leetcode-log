f3aa6f343d7033aa12f7e360d080184dcd6c867a
fvknk <97640891+fvknk@users.noreply.github.com>
2025-03-20 10:55:54 +0900

    48. Rotate Image

---
+++ history/48_Rotate_Image/20250320/src.rb
@@ -0,0 +1,24 @@
+# @param {Integer[][]} matrix
+# @return {Void} Do not return anything, modify matrix in-place instead.
+def rotate(matrix)
+  first_index = 0
+  last_index = matrix.length - 1
+
+  (first_index...last_index).each do |i1|
+    i2 = last_index - i1
+
+    (i1...i2).each do |j1|
+      j2 = last_index - j1
+
+      matrix[i1][j1],
+      matrix[j1][i2],
+      matrix[i2][j2],
+      matrix[j2][i1] = [
+        matrix[j2][i1],
+        matrix[i1][j1],
+        matrix[j1][i2],
+        matrix[i2][j2]
+      ]
+    end
+  end
+end
