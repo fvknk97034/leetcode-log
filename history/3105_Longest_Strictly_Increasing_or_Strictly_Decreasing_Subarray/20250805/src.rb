6eb3d082a33deee308158178acaf805852190e2b
fvknk97034 <97640891+fvknk@users.noreply.github.com>
2025-08-05 06:24:09 +0900

    3105. Longest Strictly Increasing or Strictly Decreasing Subarray

---
+++ history/3105_Longest_Strictly_Increasing_or_Strictly_Decreasing_Subarray/20250805/src.rb
@@ -0,0 +1,25 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def longest_monotonic_subarray(nums)
+  return 1 if nums.length === 1
+
+  result = 1
+  l = nums[0] === nums[1] ? 1 : 2
+
+  (2...nums.length).each do |i|
+    if nums[i - 1] === nums[i]
+      result = [result, l].max
+      l = 1
+      next
+    end
+
+    unless nums[i - 2] < nums[i - 1] === nums[i - 1] < nums[i]
+      result = [result, l].max
+      l = 2
+      next
+    end
+
+    l += 1
+  end
+  result = [result, l].max
+end
