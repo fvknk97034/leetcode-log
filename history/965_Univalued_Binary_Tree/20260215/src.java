056c78c0d60811c3d53aee329304075f5bee6779
fvknk97034 <97640891+fvknk97034@users.noreply.github.com>
2026-02-15 11:16:22 +0900

    965. Univalued Binary Tree

---
+++ history/965_Univalued_Binary_Tree/20260215/src.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  public boolean isUnivalTree(TreeNode root) {
+    return isSameValue(root, root.val);
+  }
+
+  private boolean isSameValue(TreeNode node, int value) {
+    if (node == null) return true;
+    if (node.val != value) return false;
+
+    return isSameValue(node.left, value) && isSameValue(node.right, value);
+  }
+}
