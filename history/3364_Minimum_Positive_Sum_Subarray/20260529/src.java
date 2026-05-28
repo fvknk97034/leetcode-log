ace483b2fa042f0711759c1078c48224fe14177a
fvknk97034 <97640891+fvknk97034@users.noreply.github.com>
2026-05-29 07:28:37 +0900

    3364. Minimum Positive Sum Subarray

---
+++ history/3364_Minimum_Positive_Sum_Subarray/20260529/src.java
@@ -0,0 +1,24 @@
+class Solution {
+  public int minimumSumSubarray(List<Integer> nums, int l, int r) {
+    int result = 1000 * r + 1;
+    int size = nums.size();
+
+    int[] preSums = new int[size + 1];
+    preSums[1] = nums.get(0);
+    for (int i = 2; i <= size; i++)
+      preSums[i] = preSums[i - 1] + nums.get(i - 1);
+
+    for (int i = 0; i < size; i++) {
+      for (int count = l; count <= r; count++) {
+        if (i + count > size)
+          break;
+
+        int sum = preSums[i + count] - preSums[i];
+        if (sum > 0 && sum < result)
+          result = sum;
+      }
+    }
+
+    return result == 1000 * r + 1 ? -1 : result;
+  }
+}
