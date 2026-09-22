86b35085d1618ef70a4a481f2ba232ae4b6711cd
fvknk97034 <97640891+fvknk97034@users.noreply.github.com>
2026-09-22 17:58:05 +0900

    2859. Sum of Values at Indices With K Set Bits

---
+++ history/2859_Sum_of_Values_at_Indices_With_K_Set_Bits/20260922/src.java
@@ -0,0 +1,11 @@
+class Solution {
+  public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+    int result = 0;
+    for (Integer i = 0; i < nums.size(); i++) {
+      if (Integer.bitCount(i) == k)
+        result += nums.get(i);
+    }
+
+    return result;
+  }
+}
